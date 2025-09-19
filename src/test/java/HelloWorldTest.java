da3fd499cbfd859c40e1da5375700006d35f7730
parammohith <86465977+parammohith@users.noreply.github.com>
2025-09-19 22:38:25 +0530

    Update HelloWorldTest.java

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,1 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HelloWorldTest {
+    @Test
+    void testAddition() {
+        assertEquals(2, 1 + 1);
+    }
+}
 
